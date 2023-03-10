d23813496a4986b6a66a35470b9c8d0bd08ba83a
lgmro <marinholgabriel@gmail.com>
2023-03-09 21:19:07 -0300

    creating routes

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const setUpApp = require('./setup')
+const setUpRoutes = require('./routes')
 
 setUpApp(app)
+setUpRoutes(app)
 
 module.exports = app
